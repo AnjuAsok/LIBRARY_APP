0f509363a657f5c0edb5ebd7b0e1863faa6f7608
AnjuAsok <anjuaalakkal@gmail.com>
2022-08-01 05:06:26 +0530

    submit3

--- BACK_END/models/user.js
+++ BACK_END/models/user.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 const bcrypt=require('bcryptjs');
 
-const config=require("../config/database")
+const config=require("../config/database");
 
 const UserSchema=mongoose.Schema({
     username:{
